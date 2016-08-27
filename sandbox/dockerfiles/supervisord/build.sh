4207e9ee49ede0be635845566ce7bccb7588aa17
ownport <ownport@gmail.com>
2016-08-27 22:34:20 +0300

    [+] supervisord

---
+++ sandbox/dockerfiles/supervisord/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z ${SANDBOX_ENV_PATH} ];
+then
+	echo '[ERROR] SANDBOX_ENV_PATH is not defined'
+	exit 1
+else
+	source ${SANDBOX_ENV_PATH%%/}/lib/build-libs.sh
+fi
+
+supervisord() {
+
+    docker build -t "ownport/supervisord:latest" \
+    	--no-cache \
+        $(get_default_args) \
+        ${SANDBOX_ENV_PATH%%/}/dockerfiles/supervisord/
+}
+
+
+$@

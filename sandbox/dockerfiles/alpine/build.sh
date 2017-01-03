093daabd6cc867423906572d8cea28f59a47bb0c
ownport <ownport@gmail.com>
2017-01-03 07:32:49 +0200

    [*] refactoring

--- sandbox/dockerfiles/alpine/build.sh
+++ sandbox/dockerfiles/alpine/build.sh
@@ -15,18 +15,26 @@ alpine_latest() {
         ${SANDBOX_ENV_PATH%%/}/dockerfiles/alpine
 }
 
-alpine_3_3() {
-    docker build -t 'ownport/alpine:3.3' \
-        $(get_default_args) \
-        --file ${SANDBOX_ENV_PATH%%/}/dockerfiles/alpine/alpine-3.3.Dockerfile \
-        ${SANDBOX_ENV_PATH%%/}/dockerfiles/alpine
+alpine_3_5() {
+	docker build -t 'ownport/alpine:3.5' \
+	$(get_default_args) \
+	--file ${SANDBOX_ENV_PATH%%/}/dockerfiles/alpine/alpine-3.5.Dockerfile \
+	${SANDBOX_ENV_PATH%%/}/dockerfiles/alpine
 }
+
 
 alpine_3_4() {
     docker build -t 'ownport/alpine:3.4' \
         $(get_default_args) \
         --file ${SANDBOX_ENV_PATH%%/}/dockerfiles/alpine/alpine-3.4.Dockerfile \
         ${SANDBOX_ENV_PATH%%/}/dockerfiles/alpine
+}
+
+alpine_3_3() {
+	docker build -t 'ownport/alpine:3.3' \
+	$(get_default_args) \
+	--file ${SANDBOX_ENV_PATH%%/}/dockerfiles/alpine/alpine-3.3.Dockerfile \
+	${SANDBOX_ENV_PATH%%/}/dockerfiles/alpine
 }
 
 $@
